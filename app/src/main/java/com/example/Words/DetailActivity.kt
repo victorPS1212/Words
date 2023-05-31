991a0c7226c161f6477da51b7b5358ae0fe01519
victorPS1212 <estudante843@gmail.com>
2023-05-31 19:33:59 -0300

    https://github.com/victorPS1212/Words.git

---
+++ app/src/main/java/com/example/Words/DetailActivity.kt
@@ -0,0 +1,28 @@
+package com.example.Words
+
+import android.os.Bundle
+import android.provider.UserDictionary.Words
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.Words.databinding.ActivityDetailBinding
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val letterId = intent?.extras?.getString("letter").toString()
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = WordAdapter(letterId, this)
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+
+        )
+        title = getString(R.string.detail_prefix) + " " + letterId
+    }
+    companion object {
+        const val LETTER = "letter"
+        const val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
+}
